07423714f104ec38a9356c92887b6a712acb439e
Manjusha Rao P <146912965+ManjushaRao@users.noreply.github.com>
2024-09-30 21:55:23 +0530

    Create pg8.cpp

---
+++ pg8.cpp
@@ -0,0 +1,11 @@
+// P8. Create an instance of Student and call print_info() on it.
+
+int main() {
+    // Create a person object with name and age
+    Person person1("Tomy", 25);
+    Student student1("Manjusha",23,"ECE");
+    person1.print_info();
+    student1.print_info();
+
+    return 0;
+}
