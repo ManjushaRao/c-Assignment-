31db456d898b3f9ee083f4b508ff82f04e945b81
Manjusha Rao P <146912965+ManjushaRao@users.noreply.github.com>
2024-09-30 16:41:43 +0530

    Create pg2.cpp

---
+++ pg2.cpp
@@ -0,0 +1,42 @@
+// P2. Create two instances of Person and call print_info() on the
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+    public:
+        // Constructor that accepts both name and age
+        Person(string name, int age) {
+            this->name = name;
+            this->age = age;
+        }
+
+        // Getter for the name
+        string getName() {
+            return name;
+        }
+
+        // Getter for the age (returns int instead of string)
+        int getAge() {
+            return age;
+        }
+
+        // Print information
+        void print_info() {
+            cout << "I am " << name << " and I am " << age << " years old." << endl;
+        }
+
+    public:
+        string name;
+        int age;
+};
+
+int main() {
+    // Create a person object with name and age
+    Person person1("Manjusha", 23);
+    Person person2("Vaishnavi",24);
+    person1.print_info();
+    person2.print_info();
+
+    return 0;
+}
