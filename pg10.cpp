61a3deb7eeb6323b4e3867498b8cd6335ed0fc03
Manjusha Rao P <146912965+ManjushaRao@users.noreply.github.com>
2024-09-30 16:45:28 +0530

    Create pg10.cpp

---
+++ pg10.cpp
@@ -0,0 +1,13 @@
+// P10. Create an instance of Teacher and call print_info() on it.
+
+int main() {
+    // Create a person object with name and age`
+    Person person1("Tony", 25);
+    Student student1("Nisha",21,"ECE");
+    Teacher teacher1("Leethesh",56,10000);
+    person1.print_info();
+    student1.print_info();
+    teacher1.print_info();
+
+    return 0;
+}
